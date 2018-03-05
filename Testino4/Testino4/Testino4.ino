bfc33d1b965cc9c9b13d09a3fcdf0795a1b424b0
Maciek-exPC <Maciek@DESKTOP-exPC>
2018-03-05 21:58:39 +0100

    Big Bang

---
+++ Testino4/Testino4/Testino4.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Testino4.ino
+ Created:	3/5/2018 9:58:19 PM
+ Author:	Maciek
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
